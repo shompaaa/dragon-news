0b7e369f8219d49110c5737730a455670345527b
shompaaa <shompaakter334@gmail.com>
2026-04-10 12:06:41 +0600

    latest news & navbar done

---
+++ src/components/LatestNews/LatestNews.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LatestNews = () => {
+  return (
+    <div className="flex gap-5 bg-base-200 items-center p-3">
+      <p className="text-base-100 bg-secondary px-3 py-2">Latest</p>
+        <p className="font-bold">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          quas.
+        </p>
+    </div>
+  );
+};
+
+export default LatestNews;
--- src/layouts/Home/Home.jsx
+++ src/layouts/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../../components/Header/Header";
+import LatestNews from "../../components/LatestNews/LatestNews";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
@@ -8,6 +10,12 @@ const Home = () => {
       <header>
         <Header></Header>
       </header>
+      <section className="w-11/12 mx-auto my-3">
+        <LatestNews></LatestNews>
+      </section>
+      <nav className="w-11/12 mx-auto my-3">
+        <Navbar></Navbar>
+      </nav>
       <main>
         <section className="left-nav"></section>
         <section className="main">
