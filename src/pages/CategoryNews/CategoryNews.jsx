3a1269419e901dfae6011cb4081b17ef6e0389b2
shompaaa <shompaakter334@gmail.com>
2026-04-10 14:54:22 +0600

    Load News Data & Filter News Data based on Category

--- src/pages/CategoryNews/CategoryNews.jsx
+++ src/pages/CategoryNews/CategoryNews.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} = useParams()
+    const allNews = useLoaderData()
+
+    const [categoryNews, setCategoryNews] = useState([])
+
+    useEffect(()=>{
+        if(id == '0'){
+            // eslint-disable-next-line react-hooks/set-state-in-effect
+            setCategoryNews(allNews)
+            return;
+        }
+        else if(id =='1'){
+            
+            const filteredNews = allNews.filter(news => news.others.is_today_pick == true)
+            setCategoryNews(filteredNews)
+        }
+        else{
+            const filteredNews = allNews.filter(news => news.category_id == id)
+                setCategoryNews(filteredNews)
+            
+        }
+    },[allNews, id])
+
     return (
         <div>
-            <h1>Category News-{id}</h1>
+
+            <h1>Total {categoryNews.length} news found</h1>
         </div>
     );
 };
--- src/routes/Routes/Routes.jsx
+++ src/routes/Routes/Routes.jsx
@@ -14,7 +14,8 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/category/:id',
-                element: <CategoryNews></CategoryNews>
+                element: <CategoryNews></CategoryNews>,
+                loader: ()=>fetch('/news.json')
             }
         ]
     },
